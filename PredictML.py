494ff326ac2cff0d66842987f13498167f88b6f2
matlockr <61714878+matlockr@users.noreply.github.com>
2020-10-20 15:58:28 -0700

    Added comments

--- PredictML.py
+++ PredictML.py
@@ -3,7 +3,12 @@ import pickle
 
 def Run(filename, featureString):
     
+    # Create numpy array from featureString and set the type to floats
     features = np.asarray(featureString.split(",")).astype(float)
+    
+    # Load the model from the modelfile
     loaded_model = pickle.load(open(filename, 'rb'))
+    
+    # Get the prediction number and return it to be displayed
     prediction = loaded_model.predict([features])
     return prediction
